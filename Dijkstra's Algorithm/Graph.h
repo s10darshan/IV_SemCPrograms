27deefcafcf0aa785d5f54819b38ecef18c3c55c
Darshan <dstha464@gmail.com>
2026-07-27 20:41:11 +0545

    added: graph structs + some function declarations

--- Dijkstra's Algorithm/Graph.h
+++ Dijkstra's Algorithm/Graph.h
@@ -1,8 +1,25 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-struct Graph{
-        int 
-}
+typedef struct AdjEdge AdjEdge;
+/* Linked List that stores adjacent edges of the vertex */
+struct AdjEdge{
+        int destination, weight;
+        AdjEdge* next;
+};
+
+/* elements of array: contains the head pointer to above linked list */
+typedef struct AdjEdgeList {
+        AdjEdge* head;
+}AdjEdgeList;
+
+typedef struct Graph{
+        int vertex;     /* the actual vertex */
+        AdjEdgeList* array;     /* the array of edges connected to/from the edges */
+};
+
+
+void createGraph(int);
+void addEdge();
 
 #endif
--- Dijkstra's Algorithm/main.c
+++ Dijkstra's Algorithm/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include "Graph.h"
+#include "Dijkstra.h"
 #define INFINITY 1e9
 
 
 
-void dijkstra(int graph[][], int source){
+// void dijkstra(int graph[][], int source){
                 
-}
+// }
 
 
 int main(){
