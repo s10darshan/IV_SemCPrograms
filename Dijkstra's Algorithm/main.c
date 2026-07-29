d2ff8beb888ca1c98a87dc95be2ebed47649ddbd
Darshan Shrestha <dstha464@gmail.com>
2026-07-29 07:28:02 +0545

    created a graph with 9 vertices from main

--- Dijkstra's Algorithm/main.c
+++ Dijkstra's Algorithm/main.c
@@ -6,6 +6,26 @@
 #include "Dijkstra.h"
 
 
-int main(){
-            return 0;
+int main() {
+    int V = 9;
+    Graph* graph = createGraph(V);
+ 
+    addEdge(graph, 0, 1, 4);
+    addEdge(graph, 0, 7, 8);
+    addEdge(graph, 1, 2, 8);
+    addEdge(graph, 1, 7, 11);
+    addEdge(graph, 2, 3, 7);
+    addEdge(graph, 2, 8, 2);
+    addEdge(graph, 2, 5, 4);
+    addEdge(graph, 3, 4, 9);
+    addEdge(graph, 3, 5, 14);
+    addEdge(graph, 4, 5, 10);
+    addEdge(graph, 5, 6, 2);
+    addEdge(graph, 6, 7, 1);
+    addEdge(graph, 6, 8, 6);
+    addEdge(graph, 7, 8, 7);
+ 
+    dijkstra(graph, 0);
+ 
+    return 0;
 }
